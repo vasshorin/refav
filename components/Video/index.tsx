d54024c81649b5f6d56ab1a4b49c45021b7f184e
Vasily <vshorin@my.bcit.ca>
2023-06-23 12:50:52 -0700

    Change desctiption

--- components/Video/index.tsx
+++ components/Video/index.tsx
@@ -13,8 +13,8 @@ const Video = () => {
     <section className="relative z-10 py-16 md:py-20 lg:py-28">
       <div className="container">
         <SectionTitle
-          title="We are ready to help"
-          paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
+          title="Some of our works"
+          paragraph=""
           center
           mb="80px"
         />
