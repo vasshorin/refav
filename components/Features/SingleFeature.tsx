75c814b1aca895dcb4c4281cbec04ec85ec0eacd
Vasily <vshorin@my.bcit.ca>
2023-06-30 15:14:27 -0700

    Remove features icons

--- components/Features/SingleFeature.tsx
+++ components/Features/SingleFeature.tsx
@@ -1,13 +1,10 @@
 import { Feature } from "@/types/feature";
 
 const SingleFeature = ({ feature }: { feature: Feature }) => {
-  const { icon, title, paragraph } = feature;
+  const { title, paragraph } = feature;
   return (
     <div className="w-full">
       <div className="wow fadeInUp" data-wow-delay=".15s">
-        <div className="mb-10 flex h-[70px] w-[70px] items-center justify-center rounded-md bg-primary bg-opacity-10 text-primary">
-          {icon}
-        </div>
         <h3 className="mb-5 text-xl font-bold text-black dark:text-white sm:text-2xl lg:text-xl xl:text-2xl">
           {title}
         </h3>
