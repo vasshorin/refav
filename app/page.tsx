5e3c83a0d63027ad7128dab8b9307896f7f5016a
Vasily <vshorin@my.bcit.ca>
2023-07-01 11:56:08 -0700

    Clean up

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,9 @@
 import AboutSectionOne from "@/components/About/AboutSectionOne";
-import AboutSectionTwo from "@/components/About/AboutSectionTwo";
-import Blog from "@/components/Blog";
-import Brands from "@/components/Brands";
 import ScrollUp from "@/components/Common/ScrollUp";
 import Contact from "@/components/Contact";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Pricing from "@/components/Pricing";
-import Testimonials from "@/components/Testimonials";
 import Video from "@/components/Video";
 import { Inter } from "@next/font/google";
 
@@ -20,13 +16,8 @@ export default function Home() {
       <Hero />
       <Features />
       <Video />
-      {/* <Brands /> */}
       <AboutSectionOne />
-      {/* <AboutSectionTwo /> */}
-      {/* <Testimonials /> */}
       <Pricing />
-      {/* <Blog /> */}
-      {/* <Contact /> */}
     </>
   );
 }
