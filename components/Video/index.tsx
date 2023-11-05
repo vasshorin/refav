4ca1ee83adcbde74659fa2e2f058dac22a1fe2ca
vasshorin <shorinvasily@gmail.com>
2023-11-04 21:34:26 -0700

    Change video frame

--- components/Video/index.tsx
+++ components/Video/index.tsx
@@ -1,10 +1,6 @@
-"use client";
-
-import Image from "next/image";
-import { useState } from "react";
-import SectionTitle from "../Common/SectionTitle";
-
-import ModalVideo from "react-modal-video";
+"use client"
+import { useState } from 'react';
+import SectionTitle from '../Common/SectionTitle';
 
 const Video = () => {
   const [isOpen, setOpen] = useState(false);
@@ -22,41 +18,24 @@ const Video = () => {
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4">
             <div
-              className="wow fadeInUp mx-auto max-w-[770px] overflow-hidden rounded-md"
+              className="wow fadeInUp mx-auto max-w-[770px] overflow-hidden rounded-md flex justify-center" // Added flex and justify-center
               data-wow-delay=".15s"
             >
-              <div className="relative aspect-[77/40] items-center justify-center">
-                <Image src="/images/video/demo-photo.png" alt="video image" fill />
-                <div className="absolute top-0 right-0 flex h-full w-full items-center justify-center">
-                  <button
-                    onClick={() => setOpen(true)}
-                    className="flex h-[70px] w-[70px] items-center justify-center rounded-full bg-white bg-opacity-75 text-primary transition hover:bg-opacity-100"
-                  >
-                    <svg
-                      width="16"
-                      height="18"
-                      viewBox="0 0 16 18"
-                      className="fill-current"
-                    >
-                      <path d="M15.5 8.13397C16.1667 8.51888 16.1667 9.48112 15.5 9.86602L2 17.6603C1.33333 18.0452 0.499999 17.564 0.499999 16.7942L0.5 1.20577C0.5 0.43597 1.33333 -0.0451549 2 0.339745L15.5 8.13397Z" />
-                    </svg>
-                  </button>
-                </div>
+              <div className="relative aspect-[16/9] w-full max-w-screen-md">
+                <iframe
+                  width="100%"
+                  height="100%"
+                  src="https://www.youtube.com/embed/FVWO8JjiA14?si=_AMeAiwIpZVqu0Pm&controls=1" // Added controls=1
+                  title="YouTube video player"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                  allowFullScreen
+                />
               </div>
             </div>
           </div>
         </div>
       </div>
-
-      <ModalVideo
-        channel="youtube"
-        autoplay={true}
-        start={true}
-        isOpen={isOpen}
-        videoId="FVWO8JjiA14"
-        onClose={() => setOpen(false)}
-      />
-
       <div className="absolute bottom-0 left-0 right-0 z-[-1]">
         <img src="/images/video/shape.svg" alt="shape" className="w-full" />
       </div>
@@ -65,3 +44,5 @@ const Video = () => {
 };
 
 export default Video;
+
+
